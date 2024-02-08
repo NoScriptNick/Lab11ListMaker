3cd3e3d7a4d0c3dac8000f3c00affe7985b38098
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-02-08 09:35:53 -0500

    aerhghr

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,46 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
+    public static ArrayList<String> list = new ArrayList<String>();
+    public static Scanner scan = new Scanner(System.in);
+    public static boolean done = false;
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<String>();
 
         String option = "";
-        boolean done = false;
-
+        list.addAll(Arrays.asList("Mr. Boolean", "Nick Beres", "Sub 5 minute mile", "LCL Victory", "Physics", "Books", "Math", "ReallyLongString", "Gamestop", "Barnes and Noble"));
         while (!done) {
-            list.addAll(Arrays.asList("Mr. Boolean", "Nick Beres", "Sub 5 minute mile", "LCL Victory", "Physics", "Books", "Math", "ReallyLongString", "Gamestop", "Barnes and Noble"));
 
-            InputHelper.getRegExString(scan, "Options: \nA – Add an item to the list \nD – Delete an item from the list \nP – Print the list \nQ – Quit the program", "AaDdPpQq");
-
+            option = InputHelper.getRegExString(scan, "Options: \nA – Add an item to the list \nD – Delete an item from the list \nP – Print the list \nQ – Quit the program", "[AaDdPpQq]");
+            if (option.equalsIgnoreCase("A")) {
+                add();
+            } else if (option.equalsIgnoreCase("D")) {
+                delete();
+            } else if (option.equalsIgnoreCase("P")) {
+                print();
+            } else if (option.equalsIgnoreCase("Q")) {
+                quit();
+            }
         }
     }
 
-    public static ArrayList<String> Add(ArrayList<String> list){
-        ArrayList<String> temp = list;
-
-        return temp;
+    public static void add() {
+        String bruh = InputHelper.getNonZeroLenString(scan, "ADD SOMETHING AND DO IT QUICK");
+        list.add(bruh);
     }
 
-    public static ArrayList<String> Delete(ArrayList<String> list){
-        ArrayList<String> temp = list;
-
-        return temp;
+    public static void delete(){
+        int index = InputHelper.getRangedInt(scan, "ENTER AN INDEX BEFORE I LOSE MY PATIENCE", 0, list.size());
+        list.remove(index);
     }
 
-    public static ArrayList<String> Print(ArrayList<String> list){
-        ArrayList<String> temp = list;
-
-        return temp;
+    public static void print(){
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i + " - " + list.get(i));
+        }
     }
 
-    public static boolean Quit(){
-        Scanner scan = new Scanner(System.in);
-        boolean done = InputHelper.getYNConfirm(scan, "Are you sure you want to quit? [Y/N]");
-        return done;
+    public static void quit(){
+        done = InputHelper.getYNConfirm(scan, "Are you sure you want to quit? [Y/N]");
     }
 }
